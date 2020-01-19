70b467a7630c36da916ea57ed3ec69c8f932d951
John Heerman <john.heerman@agcocorp.com>
2020-01-19 11:24:02 -0600

    Initial Commit

---
+++ capture.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def capture_screen(url, screenshot_path):
+    logger.debug('prepping chromedriver')
+
+    """ 
+    Note: the path to the chromedriver must be added to the system PATH: export PATH:=$PATH:${pwd} 
+    Download the chromedriver from: https://sites.google.com/a/chromium.org/chromedriver/
+    """
+    DRIVER = 'chromedriver'
+    driver = webdriver.Chrome(DRIVER)
+    logger.debug('requesting url')
+    driver.get(url)
+    logger.debug('saving screenshot')
+    screenshot = driver.save_screenshot(screenshot_path)
+    logger.debug('done')
+    driver.quit()
+
+if __name__ == "__main__":
+    url = 'https://www.google.com'
+    screenshot_path = 'google_homepage.png'
+    logger.debug(f"Requesting page {url} and saving {screenshot_path}")
+    capture_screen(url, screenshot_path)
